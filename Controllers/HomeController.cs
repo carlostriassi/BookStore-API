6ebd3ecf6e0719f2dbf8e63adb7da727ec1e0793
Carlos Triassi <triassicarlos@yahoo.com>
2021-01-14 22:23:37 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+
+
+    /// <summary>
+    /// this is a test API controller
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Access home controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>Gets the specified identifier.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
+        [HttpGet("{id}")]
+        public string Get(int id)   
+        {
+            _logger.LogDebug("Get a value");
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("This is an error");
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("This is an warning");
+        }
+    }
+}
